66585ba18ebd4128504241a732af4cdc4cb33c75
Jim Wisniewski <wisnij@gmail.com>
2022-11-28 22:46:33 -0500

    conversion arguments

--- convert.sh
+++ convert.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -eu
+cd "$(dirname "$0")"
 
 sizes=(22 48 128)
 weathers=(
@@ -24,11 +26,19 @@ weathers=(
     sun
 )
 
+icons=("$@")
+if [[ ${#icons[@]} -eq 0 ]]; then
+    for weather in ${weathers[@]}; do
+        icons+=("$weather" "$weather-night")
+    done
+fi
+
 convert_icon () {
     icon=$1
     icon_src="svg/$icon.svg"
     if [[ ! -e $icon_src ]]; then
-        return
+        echo "$icon_src: no such file" >&2
+        return 1
     fi
 
     for size in ${sizes[@]}; do
@@ -38,12 +48,19 @@ convert_icon () {
         fi
 
         dest="$size/$icon.png"
-        echo "$size_src -> $dest"
+        echo "$size_src: wrote $dest"
         convert -size "${size}x${size}" -background none "$size_src" "$dest"
     done
 }
 
-for weather in ${weathers[@]}; do
-    convert_icon "$weather"
-    convert_icon "$weather-night"
+converted=0
+for icon in "${icons[@]}"; do
+    if convert_icon "$icon"; then
+        converted=$((converted + 1))
+    fi
 done
+
+if [[ $converted -eq 0 ]]; then
+    echo "no icons converted!" >&2
+    exit 1
+fi
